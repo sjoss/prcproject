480d225c2a12c4843ff213c8a6856903cf950926
sjoss <dark.diybio@gmail.com>
2016-09-25 17:42:56 +0200

    Add files via upload

---
+++ Arduino/build/Debug/Arduino-Windows/Pcr.o.d
@@ -0,0 +1,39 @@
+build/Debug/Arduino-Windows/Pcr.o: Pcr.cpp \
+  D:/prog/Arduino/libraries/SPI/SPI.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Arduino.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/binary.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/WCharacter.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/WString.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Stream.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Print.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Printable.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/new.h \
+  D:/prog/Arduino/hardware/arduino/variants/standard/pins_arduino.h Pcr.h \
+  D:/prog/Arduino/libraries/Adafruit_MAX31855/Adafruit_MAX31855.h
+
+D:/prog/Arduino/libraries/SPI/SPI.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Arduino.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/binary.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/WCharacter.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/WString.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Stream.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Print.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Printable.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/new.h:
+
+D:/prog/Arduino/hardware/arduino/variants/standard/pins_arduino.h:
+
+Pcr.h:
+
+D:/prog/Arduino/libraries/Adafruit_MAX31855/Adafruit_MAX31855.h:
---
+++ Arduino/build/Debug/Arduino-Windows/main.o.d
@@ -0,0 +1,41 @@
+build/Debug/Arduino-Windows/main.o: main.cpp \
+  D:/prog/Arduino/libraries/SPI/SPI.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Arduino.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/binary.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/WCharacter.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/WString.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Stream.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Print.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/Printable.h \
+  D:/prog/Arduino/hardware/arduino/cores/arduino/new.h \
+  D:/prog/Arduino/hardware/arduino/variants/standard/pins_arduino.h \
+  D:/prog/Arduino/libraries/LCD4884/LCD4884.h Pcr.h MenuParam.h
+
+D:/prog/Arduino/libraries/SPI/SPI.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Arduino.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/binary.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/WCharacter.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/WString.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Stream.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Print.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/Printable.h:
+
+D:/prog/Arduino/hardware/arduino/cores/arduino/new.h:
+
+D:/prog/Arduino/hardware/arduino/variants/standard/pins_arduino.h:
+
+D:/prog/Arduino/libraries/LCD4884/LCD4884.h:
+
+Pcr.h:
+
+MenuParam.h:
